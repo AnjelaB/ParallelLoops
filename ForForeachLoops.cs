a6653a0f47ea180e7cbafce91884f59bca42c932
AnjelaB <36104637+AnjelaB@users.noreply.github.com>
2018-04-15 00:14:39 +0400

    Add files via upload

---
+++ ForForeachLoops.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    public class ForForeachLoops
+    {
+        /// <summary>
+        /// The For loop that's blocks would be done parallel.
+        /// </summary>
+        /// <param name="fromInclusive">The Start index.</param>
+        /// <param name="toExclusive">The End index.</param>
+        /// <param name="body">Body of the loop.</param>
+        public static void ParallelFor(int fromInclusive, int toExclusive, Action<int> body)
+        {
+            if(fromInclusive > toExclusive || body == null)
+            {
+                throw new Exception("You give either wrong indzexes, or there isn't body.");
+            }
+            // We want to create max 10 tasks.
+            int max = toExclusive - fromInclusive;
+            if (max > 10)
+            {
+                max = 10;
+            }
+
+            var tasks = new Task[max];
+            for(int i = fromInclusive; i <= toExclusive; i++)
+            {
+                var iondexOfArray = i;
+                if (iondexOfArray < max)
+                {
+                    tasks[i] = new Task(() => body(iondexOfArray));
+                    tasks[i].Start();
+                }
+                else
+                {
+                    var indexOfTask = Task.WaitAny(tasks);
+                    tasks[indexOfTask] = new Task(() => body(iondexOfArray));
+                    tasks[indexOfTask].Start();
+                }
+            }
+            Task.WaitAll(tasks);
+        }
+
+        /// <summary>
+        /// The ForEach loop that's blocks would be done parallel.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="body"></param>
+        public static void ParallelForEach<TSource>(IEnumerable<TSource> source, Action<TSource> body)
+        {
+            if (source == null || body == null)
+            {
+                throw new Exception("Either source is null, or body");
+            }
+            int countOfWorkingBlocks = 0;
+            int max = source.Count();
+            if (max > 10)
+            {
+                max = 10;
+            }
+            var elem = new Task[max];
+            foreach (var element in source)
+            {
+                
+                var indexOfElement = countOfWorkingBlocks;
+                countOfWorkingBlocks++;
+                var current = element;
+                if(indexOfElement >= max)
+                {
+                    var indexofTask = Task.WaitAny(elem);
+                    elem[indexofTask] = new Task(() => body(current));
+                    elem[indexofTask].Start();
+                }
+                else
+                {
+                    elem[indexOfElement] = new Task(() => body(current));
+                    elem[indexOfElement].Start();
+                }
+
+            }
+            Task.WaitAll(elem);
+        }
+
+        /// <summary>
+        /// Method ForEach Parallel with Options.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="parallelOptions"></param>
+        /// <param name="body"></param>
+        public static void ParallelForEachWithOptions<TSource>(IEnumerable<TSource> source, ParallelOptions parallelOptions, Action<TSource> body)
+        {
+            if(source == null)
+            {
+                throw new Exception("Source is null.");
+            }
+            if (parallelOptions == null)
+            {
+                throw new Exception("parallelOptions is null.");
+            }
+            if (body == null)
+            {
+                throw new Exception("Body is null");
+            }
+
+            int countOfWorkingBlocks = 0;
+            int max = source.Count();
+            if (max > parallelOptions.MaxDegreeOfParallelism)
+            {
+                max = parallelOptions.MaxDegreeOfParallelism;
+            }
+
+            TaskFactory taskFactory = new TaskFactory(parallelOptions.CancellationToken, TaskCreationOptions.None, TaskContinuationOptions.None, parallelOptions.TaskScheduler);
+            var elem = new Task[max];
+            foreach (var element in source)
+            {
+
+                var indexOfElement = countOfWorkingBlocks;
+                countOfWorkingBlocks++;
+                var current = element;
+                if (indexOfElement >= max)
+                {
+                    var indexofTask = Task.WaitAny(elem);
+                    elem[indexofTask] = taskFactory.StartNew(() => body(current), parallelOptions.CancellationToken);
+                }
+                else
+                {
+                    elem[indexOfElement] = new Task(() => body(current));
+                    elem[indexOfElement].Start();
+                }
+                
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Loops;
+
+namespace ParallelLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<int> list = new List<int>();
+            int[] numbers = new int[20];
+
+            for (int i = 0; i < 20; i++)
+            {
+                list.Add(rnd.Next(1, 200));
+                numbers[i] = i;
+                Console.WriteLine(list.Last());
+            }
+
+            ForForeachLoops.ParallelFor(0, 19, num => Console.WriteLine(numbers[num]));
+            Console.WriteLine();
+            ForForeachLoops.ParallelForEach<int>(list, elem => Console.WriteLine(list.IndexOf(elem) + " - " + elem));
+
+            ParallelOptions parallelOptions = new ParallelOptions();
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            parallelOptions.MaxDegreeOfParallelism = 20;
+            parallelOptions.CancellationToken = token;
+
+            ForForeachLoops.ParallelForEachWithOptions<int>(list, parallelOptions, (elem) =>Function(list,elem,token));
+
+            Console.WriteLine("Enter 'c' if you want cancel a program.");
+            var el=Console.ReadLine();
+            if(el=="c" || el == "C")
+            {
+                tokenSource.Cancel();
+            }
+            Console.ReadLine();
+            
+        }
+
+        public static void Function(List<int> list,int elem,CancellationToken token)
+        {
+            if (token.IsCancellationRequested == true)
+            {
+                Console.WriteLine("Task was cancelled before it got started.");
+                token.ThrowIfCancellationRequested();
+            }
+            else
+            {
+                Console.WriteLine(list.IndexOf(elem) + " - " + elem);
+                Thread.Sleep(1000);
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Task cancelled");
+                    token.ThrowIfCancellationRequested();
+                }
+
+            }
+        }
+    }
+}
